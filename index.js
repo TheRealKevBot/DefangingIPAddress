5b507728d9e316199a3b3a6ce3d81940bd4d98b4
TheRealKevBot <Kglas001@gmail.com>
2021-06-22 19:04:06 -0600

    First

---
+++ index.js
@@ -0,0 +1,13 @@
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]". 
+
+function defangIPaddr(address) {
+    
+}; 
+
+console.log(defangIPaddr('1.1.1.1'))
+console.log(defangIPaddr('255.100.50.0'))
+console.log(defangIPaddr('100.120.431.10'))
+console.log(defangIPaddr('761.91.314.100'))
+console.log(defangIPaddr('100.100.100.100'))
